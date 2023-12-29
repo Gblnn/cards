e4bfe84cbfca00425159e28b4a536b64e681a062
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-12-29 22:25:52 +0400

    update

--- components/card.tsx
+++ components/card.tsx
@@ -1,17 +1,10 @@
-import { Animated, Image, StyleSheet, Text, View } from "react-native";
-import data from "../data";
+import { StyleSheet, View } from "react-native";
 
 export default function Card({card}:{card:any}){
     return(
-        <Animated.View style={styles.card}>
-            <View style={styles.header}>
-                <Image style={styles.header_item} source={require('../assets/icons/tick.png')}/>
-                <Image style={styles.header_item} source={require('../assets/icons/cross.png')}/>
-            </View>
-            <View style={[styles.body, {backgroundColor:card.color}]}>
-                {/* <Image style={styles.image} source={{uri:card.image}}/> */}
-            </View>
-        </Animated.View>
+        <View style={styles.card}>
+            <View style={[styles.body, {backgroundColor:card.color}]}/>
+        </View>
     )
 }
 
@@ -20,7 +13,6 @@ const styles = StyleSheet.create({
         flex:0.75,
         marginTop:100,
         width: "100%",
-        height:500,
         backgroundColor:"white",
         borderRadius:20,
         shadowOffset: {
@@ -31,38 +23,11 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         
     },
-    header:{
-        position:"absolute",
-        flexDirection:"row",
-        justifyContent:"space-between",
-        padding:25,
-        zIndex:1,
-        width:"100%"
-        // borderWidth:1.5,
-        
-    },
-    header_item:{
-        // borderWidth:1.5,
-        borderRadius:5,
-        justifyContent:"center",
-        alignItems:"center",
-        width:30,
-        height:30,
-        display:"none"
-    },
     body:{
         flex:1,
         // borderWidth:1,
         justifyContent:"center",
         alignItems:"center",
         borderRadius:20,
-        
-    },
-    image:{
-    
-        width:"100%",
-        height:"100%",
-        borderRadius:20,
-        resizeMode:"contain"
     }
   });
--- data.ts
+++ data.ts
@@ -1,27 +1,22 @@
 export default [
     {
         id:"1",
-        image:"",
         color:"#92cdf3"
     },
     {
         id:"2",
-        image:"",
         color:"#80e7ba"
     },
     {
-        id:"3",
-        image:"",
+        id:"3",   
         color:"#fcd689"
     },
     {
         id:"4",
-        image:"",
         color:"#d48c65"
     },
     {
         id:"5",
-        image:"",
         color:"#c05d62"
     }
 
